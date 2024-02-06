aa228e5a4c6d0fc2ccc5a45da17bdb2f9f857138
Isai Torres Saucedo <isato122@gmail.com>
2024-02-06 14:57:11 -0600

    mejorando funciones

--- logica-programacion/logica-programacion-2-fun-list/logica-programacion-2-Aula1/app.js
+++ logica-programacion/logica-programacion-2-fun-list/logica-programacion-2-Aula1/app.js
@@ -1,3 +1,4 @@
+//forma de colocar texto a etiquetas html
 // let titulo = document.querySelector('h1')
 // titulo.innerHTML = "Adivina el número secreto";
 
@@ -14,84 +15,104 @@ const btnIntentar = document.getElementById("btnIntentar");
 const btn = document.getElementById("btn__iniciar");
 const btnReiniciar = document.getElementById("reiniciar");
 let input = document.getElementById("numeroMaximoJuego");
+let numeroIntento;
+let numeroMaximoUsuario;
+let oportunidades;
 
 //se crea funcion para agregar etiquetas html desde js
-function crearEtiquetas(etiquetaHtml,  contenidoEtiqueta){
-    etiquetaHtml = document.querySelector(etiquetaHtml)
-    etiquetaHtml.innerHTML = contenidoEtiqueta;
+function crearEtiquetas(etiquetaHtml, contenidoEtiqueta) {
+  etiquetaHtml = document.querySelector(etiquetaHtml);
+  etiquetaHtml.innerHTML = contenidoEtiqueta;
 }
-crearEtiquetas("h1","Adivina el número secreto");
-crearEtiquetas("h2","Instrucciones");
-crearEtiquetas('.texto__parrafo',"Introduce el numero máximo con el que quieras jugar (entre 50 y 100), tendras hasta 10 intentos para adivinar el número aleatorio que sera desde 10 y hasta el número indicado.")
+//creando etiquetas html
+crearEtiquetas("h1", "Adivina el número secreto");
+crearEtiquetas("h2", "Instrucciones");
+crearEtiquetas(
+  ".texto__parrafo",
+  "Introduce el numero máximo con el que quieras jugar (entre 50 y 100), tendras hasta 10 intentos para adivinar el número aleatorio que sera desde 1 y hasta el número indicado."
+);
 
+// agregando evento click a boton
+btn.addEventListener("click", generaNumeroAleatorio);
 
+//Se crea un numero aleatorio utilizanodo el numero maximo indicado por el usuario
+function generaNumeroAleatorio() {
+  numeroMaximoUsuario = parseInt(
+    document.getElementById("numeroMaximoJuego").value
+  );
+  numeroAleatorio = Math.floor(Math.random() * (numeroMaximoUsuario + 1));
+  crearEtiquetas(
+    "p",
+    `¡introduce un número entre 1 y ${numeroMaximoUsuario}, tendras ${
+      numeroMaximoUsuario === 50 ? " 5 oportunidades " : " 10 oportunidades"
+    }!`
+  );
+  borrarInput();
+  console.log(`El numero aleatorio es ${numeroAleatorio}`);
 
+  // Deshabilitar el botón
+  btn.disabled = true;
+  btn.style.backgroundColor = "gray";
 
-//Se crea un numero aleatorio utilizanodo el numero maximo indicado por el usuario 
+  // Deshabilitar el campo de entrada
 
+  input.disabled = true;
+  input.style.backgroundColor = "gray";
 
-
-btn.addEventListener("click",generaNumeroAleatorio)
-
-function generaNumeroAleatorio(){
-    let numeroMaximoUsuario = parseInt(document.getElementById("numeroMaximoJuego").value);
-    numeroAleatorio = Math.floor(Math.random()* (numeroMaximoUsuario + 1))
-    crearEtiquetas("p", `¡introduce un número entre 1 y ${numeroMaximoUsuario}, tendras ${numeroMaximoUsuario === 50 ? " 5 oportunidades " : " 10 oportunidades"}!`)
-    borrarInput();
-    console.log(`El numero aleatorio es ${numeroAleatorio}`)
-    // Deshabilitar el botón
-    btn.disabled = true;
-    btn.style.backgroundColor = "gray";
-
-    // Deshabilitar el campo de entrada
-    
-    input.disabled = true;
-    input.style.backgroundColor = "gray";
-
-    return numeroAleatorio;   
+  return numeroAleatorio;
 }
 
 //creando funcion del boton iniciar
-function verificarIntento(){
-    const numeroIntento = parseInt(document.getElementById("numeroUsuario").value);
-    // console.log("funcion del boton intentar")
-    // console.log(`${numeroIntento}`)
-    // console.log(numeroAleatorio)
-    // console.log(numeroIntento == numeroAleatorio)
+function verificarIntento() {
+  numeroIntento = parseInt(document.getElementById("numeroUsuario").value);
 
-    if (numeroIntento === numeroAleatorio) {
-        crearEtiquetas("p", `¡Correcto acertaste en ${intentos} ${(intentos === 1) ? "intento" : "intentos"}!`)
-        crearEtiquetas("h2","");
-        btnNuevoJuego();
+  if (numeroIntento === numeroAleatorio) {
+    crearEtiquetas(
+      "p",
+      `¡Correcto acertaste en ${intentos} ${
+        intentos === 1 ? "intento" : "intentos"
+      }!`
+    );
+    crearEtiquetas("h2", "");
+    btnNuevoJuego();
+  } else {
+    if (numeroIntento > numeroAleatorio) {
+      crearEtiquetas("p", "El número secreto es menor");
+      crearEtiquetas("h2", "");
     } else {
-        if (numeroIntento > numeroAleatorio) {
-            crearEtiquetas("p", "El número secreto es menor")
-            crearEtiquetas("h2","");
-        }else{
-            crearEtiquetas("p", "El número secreto es mayor")
-            crearEtiquetas("h2","");
-        }
-        intentos++;
-        borrarInput();
+      crearEtiquetas("p", "El número secreto es mayor");
+      crearEtiquetas("h2", "");
     }
+    intentos++;
+    borrarInput();
+  }
 }
 
 function borrarInput() {
-    document.getElementById("numeroMaximoJuego").value = "";
-    document.getElementById("numeroUsuario").value = "";
+  document.getElementById("numeroMaximoJuego").value = "";
+  document.getElementById("numeroUsuario").value = "";
 }
 
 function btnNuevoJuego() {
-    btnIntentar.disabled = true;
-    btnReiniciar.disabled = false; 
+  btnIntentar.disabled = true;
+  btnReiniciar.disabled = false;
 }
 
-btnReiniciar.addEventListener("click",() => {
-    btn.disabled = false;
-    btn.style.backgroundColor = "#1875e8";
-    input.disabled = false;
-    input.style.backgroundColor = "white";
-    input.focus;
+btnReiniciar.addEventListener("click", () => {
+  btn.disabled = false;
+  btn.style.backgroundColor = "#1875e8";
+  input.disabled = false;
+  input.style.backgroundColor = "white";
+  input.focus();
+  crearEtiquetas("h2", "Instrucciones");
+  crearEtiquetas(
+    ".texto__parrafo",
+    "Introduce el numero máximo con el que quieras jugar (entre 50 y 100), tendras hasta 10 intentos para adivinar el número aleatorio que sera desde 1 y hasta el número indicado."
+  );
+  borrarInput();
+  btnIntentar.disabled = false;
+  btnReiniciar.disabled = true;
+  intentos = 1;
 });
 //asignando funcion al boton iniciar
-btnIntentar.addEventListener("click",verificarIntento);
+btnIntentar.addEventListener("click", verificarIntento);
